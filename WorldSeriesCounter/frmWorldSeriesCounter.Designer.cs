3ff55f16145472dae9b15c0c5e1eb03a08cbbd13
zbiggy1982 <wmitchell1982@gmail.com>
2014-01-14 15:43:11 -0700

    commit

---
+++ WorldSeriesCounter/frmWorldSeriesCounter.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace WorldSeriesCounter
+{
+    partial class frmWorldSeriesCounter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbTeams = new System.Windows.Forms.ListBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbTeams
+            // 
+            this.lbTeams.FormattingEnabled = true;
+            this.lbTeams.Location = new System.Drawing.Point(12, 12);
+            this.lbTeams.Name = "lbTeams";
+            this.lbTeams.Size = new System.Drawing.Size(254, 160);
+            this.lbTeams.TabIndex = 0;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(12, 189);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 13);
+            this.lblResult.TabIndex = 1;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(12, 226);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(254, 23);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Calculate World Series Wins for Selected Team";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // frmWorldSeriesCounter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lbTeams);
+            this.Name = "frmWorldSeriesCounter";
+            this.Text = "World Series Counter";
+            this.Load += new System.EventHandler(this.frmWorldSeriesCounter_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbTeams;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ WorldSeriesCounter/frmWorldSeriesCounter.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+ * Program Name: World Series Counter Ver 1.0
+ * Programers: William Mitchell and Kyle Cantor
+ * Course: POS/409
+ * Instructor: Dr Brad Purdy
+ * Date: January 9, 2014
+ * Description: Calculates number of times a team has won the World Series
+ * */
+namespace WorldSeriesCounter
+{
+    public partial class frmWorldSeriesCounter : Form
+    {
+        public frmWorldSeriesCounter()
+        {
+            InitializeComponent();
+        }
+        
+        //creates method for reading the contents of a file to an array
+        private ArrayList fileToArrayList(string filename)
+        {
+            StreamReader file;
+            ArrayList list = new ArrayList();
+            file = File.OpenText(filename);
+
+            while (!file.EndOfStream)
+            {
+                list.Add(file.ReadLine());
+            }
+            file.Close();
+            return list;
+        }
+
+        //on load populates the list box with the teams.txt file
+        private void frmWorldSeriesCounter_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                ArrayList teams = fileToArrayList("Teams.txt");
+                foreach (string value in teams)
+                {
+                    this.lbTeams.Items.Add(value);
+                }
+                                
+            }
+            catch (Exception x)
+            {
+                MessageBox.Show(x.Message);
+            }
+        }
+
+        //on button click calculates the number of times a team has won and displays the results
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ArrayList winners = fileToArrayList("WorldSeries.txt");
+                string selection = this.lbTeams.SelectedItem.ToString();
+                int teamCount = 0;
+                foreach (string value in winners)
+                {
+                    if (value == selection)
+                    {
+                        teamCount++;
+                    }
+                }
+                this.lblResult.Text = "The " + selection + " have won " + teamCount + " World Series";
+            }
+            catch (Exception x)
+            {
+                MessageBox.Show(x.Message);
+            }
+        }
+    }
+}
